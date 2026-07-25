cb0d2e08b7d1ad90843a3bbc4788efd8d6b5be81
Nickotech101 <se.aiml.nikita@gmail.com>
2026-07-25 13:40:27 +0530

    Frontend setup and project structure

---
+++ frontend/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-[#09090B] text-white">
+      Something went wrong.
+    </div>
+  );
+}
---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-[#09090B] text-white">
+      404 | Page Not Found
+    </div>
+  );
+}
